c38d610d3d9507e4ea8b841b816d7a5d1456e7cd
auleki <kodagiwa@gmail.com>
2021-06-28 12:33:43 +0100

    now able to close menu on nav link select

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 // SHOW MENU
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close')
+  navClose = document.getElementById('nav-close'),
+  navLinks = document.querySelectorAll('.nav__link')
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
@@ -14,3 +15,7 @@ if (navClose) {
     navMenu.classList.remove('show-menu')
   })
 }
+
+// CLOSE MENU ON NAVLINK SELECT
+const closeMenu = () => navMenu.classList.remove('show-menu')
+navLinks.forEach(link => link.addEventListener('click', closeMenu))
