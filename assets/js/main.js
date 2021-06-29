904d4db192fa3f3b301258a0a77266f1bc3830f4
auleki <kodagiwa@gmail.com>
2021-06-29 19:13:06 +0100

    added skills section [-] bug with accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close'),
-  navLinks = document.querySelectorAll('.nav__link')
+  navLinks = document.querySelectorAll('.nav__link'),
+  skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header')
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
@@ -19,3 +21,17 @@ if (navClose) {
 // CLOSE MENU ON NAVLINK SELECT
 const closeMenu = () => navMenu.classList.remove('show-menu')
 navLinks.forEach(link => link.addEventListener('click', closeMenu))
+
+// ACCORDION SKILLS
+function toggleSkills () {
+  let itemClass = this.parentNode.className
+  // console.log('CLICKING MENu')
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skill__content skills_close'
+  }
+  if (itemClass === 'skill__content skill_close') {
+    this.parentNode.className = 'skill__content skills_open'
+  }
+}
+
+skillsHeader.forEach(el => el.addEventListener('click', toggleSkills))
